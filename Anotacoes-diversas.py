209c1d76aae5392e8bd4164979962631a8e89dfb
Marcia-Moreira <mm.no.linkedin@gmail.com>
2024-01-24 22:41:05 -0300

    Commit inicial 24-01-24

---
+++ Anotacoes-diversas.py
@@ -0,0 +1,13 @@
+# Links de Sites que podem ajudar:
+
+
+# ENTREVISTAS:
+
+# Metodologia STAR na Entrevista:
+# https://secandidatemulher.com.br/blog/aprenda-a-aplicar-a-metodologia-star-na-entrevista-de-emprego/
+
+# A técnica STAR (situação, tarefa, ação e resultado):
+# -Qual foi a situação mais desafiadora que você já vivenciou nesses anos de trabalho? 
+# -Você consegue se lembrar de alguma situação difícil com o cliente onde foi necessário que você pensasse em uma -solução? 
+# -Descreva um momento em que você estava sob muita pressão no trabalho. 
+# -Você pode me contar um exemplo em que você soube usar a adaptabilidade? 
---
+++ aulas_SCM_Academy_2024/anotacoes_scm_academy_2024.js
@@ -0,0 +1,238 @@
+// anotacoes_scm_academy_2024.js
+
+// 000 -> Módulo -> Comece por aqui!
+// Apresentação SCM Academy
+
+// 001 -> Módulo -> Mapa da Recolocação
+
+// Proceso Seletivo é mais que ter um bom Currículo
+
+// ----------------------------------------------------------------
+// 01 Etapas/Fluxos de um Processo Seletivo:
+
+// -Triagem de Perfil 
+// -Entrevista por competências 
+// -Case ou teste técnico
+// -Entrevista com time técnico 
+// -Entrevista com Líder ou diretoria 
+// -Entrevista de offer e negociação 
+
+// ----------------------------------------------------------------
+// 02 Mapa da Recolocação:
+
+// Autoconhecimento:    
+// 1 Eu sei minhas forças?   
+// 2 Consigo saber em quais áreas me destaco?
+// 3 Sei onde preciso me aperfeiçoar? Está fazendo isso?
+
+// Marca Pessoal:
+// 1 Consigo mostrar quem eu sou? 
+// 2 Meu linkedIn conta minha jornada profissional?
+// 3 Meu currículo está focado no meu objetivo?
+
+// Busca de Vaga:
+// 1 Sei buscar as vagas? 
+// 2 Consigo abordar recrutadores?
+// 3 Sei fazer a aplicação das vagas? (preencher forms das plataformas de forma otimizada?)
+
+// Apresentação Pessoal:
+// 1 Consigo resumir minha história de forma clara e interessante?
+// 2 Consigo gravar um vídeo sobre mim?
+// 3 Fico tranquila ao responder perguntas sobre mim?
+
+// ----------------------------------------------------------------
+// 03 Procurar trabalho dá trabalho:
+
+// Autoconhecimento -> minha história
+// Atenção aos prazos -> como o recrutador quer a execução das fases 
+// Se preparar com antecedência para estar pronto quando chamado
+
+// 002 -> Módulo -> Sindrome do Impostor
+
+// ----------------------------------------------------------------
+// 04 A Síndrome do Impostor e por que ela impacta no processo seletivo?
+
+// Aprendemos habilidades desde sempre (esporte, aula de artesanato)
+// Acontece mais em mulheres
+// Homens correm mais riscos
+// Mulheres devem se candidatar mesmo sem ter os 100%
+
+// ----------------------------------------------------------------
+// 05 Atividade Linha do Tempo (pdf)
+
+// LINHA DO TEMPO
+
+// A técnica da biografia, consiste em você escrever sua história através de uma linha do tempo.
+// Nessa atividade, você irá focar nos 10 marcos mais importantes da sua vida. Após reconhecer esses marcos chegou a hora de
+// colocar no papel e escrever a sua autobiografia. Quem é você?
+// Primeiro você faz uma linha do tempo e nela você coloca seus marcos de vida, de formação e de carreira. Provavelmente, você
+// irá se surpreender com o quando você já realizou e o quanto se desenvolveu.
+
+// |--Nº--|--Ano que Aconteceu--|--Acontecimento--|--Principal Aprendizado--|
+
+// 1 - 2003 - Conclusão da Faculdade de Adm -  Promoção de Trabalho e não de salário. Aprendi as dores de não respeitar a minha carreira
+// 2 - 2010 - Fiquei viúva aos 29 anos - Aprendi que sou uma mulher forte
+// 3 - 2018 - Primeira transição de carreira - Totalmente sem consciência, apanhei bastante mas sobrevivi
+// 4 - 2018 - Casei pela segunda vez - Aprendi que posso ter uma segunda chance de amar
+// 5 - 2023 - Segunda transição de Carreira - Aprendi as dores de estar 40+
+// 6 - 2017 - Decidi mudar para SP - Senti muita falta da família e tive que aprender a conviver comigo mesma! rs
+// 7 - 2023 - Corri minha primeira Maratona - Aprendi que com 42 anos podemos sim superar nossos limites e dores.
+// 8 - 2021 - PósGraduação - Durante a pandemia, estudei e fiz minha primeira POS para ajudar a superar o isolamento.
+// 9 -
+// 10 -
+
+// * Obs: em 11-01-2024, não consegui escrever pontos positivos da minha tragetória de VideoFrame. Tentar refazer depois pra ver se melhora. 
+
+// ----------------------------------------------------------------
+// 06 Comportamento de quem se sente Impostor no Trabalho: 
+
+// 1-Perfeccionismo extremo 
+// 2-Achar que sabe insuficientemente sempre 
+// 3-Achar que não pode precisar de ajuda 
+// 4-Achar que dificuldade é para pessoa incompetente 
+// 5-O sucesso é pura sorte 
+// 6-Trabalho é seu sobrenome (eu não) 
+// 7-Autossabotagem 
+// 8-Sua única habilidade é a social
+
+// ----------------------------------------------------------------
+// 07 Principais tipos e Sindrome do Impostor (são 5)
+
+// - O perfecconista
+// - O Super Homem ou Super Mulher 
+// - O solitário
+// - A gênia natural 
+// - A especialista
+
+// ----------------------------------------------------------------
+// *08 Teste dos Sabotadores:
+//! Pendente:
+// https://conteudo.lec.com.br/teste-dos-sabotadores
+// Acessar o site acima para fazer o teste!!!
+// E preencher o forms
+
+// ----------------------------------------------------------------
+// 09 Como lidar com a Sindrome do Impostor:
+
+// Reforços Positivos
+// O que mandou bem 
+// Caixinha do Mandou bem!
+// Vocalize - Falar o que teve de positivo o que fez bem!
+// Se desafie a sair desse buraco
+// Se organizar com chequelist tudo que quer fazer e observe o andamento
+// Reconhecendo a nossa história e do que sou capaz
+
+// ----------------------------------------------------------------
+// 10 Autobiografia: 
+
+// * Escrever e enviar na Plataforma!!! Autoconhecimento - Autobiografia!!!
+
+//! EXERCÍCIO DA AUTOBIOGRAFIA
+// A técnica da autobiografia, consiste  em  vocêescrever  sua  história...  Você  pode  escrevercomo  se  sentir  mais  à  vontade...  Separandopor anos, fatos ou texto corrido. Ser detalhistaé  bem  importante,  pois  sua  história  é  o  seugrande diferencial nos processos seletivos.
+// Essa  atividade  é  super  desafiadora,  uma  vezque nós não somos acostumados a olhar paranós mesmos, contar e valorizar nossa história.Então, se acolha e mãos à obra!
+// Nela você coloca seus marcos de vida, desde asua infância, adolescência, formação, de vidae  de  carreira.  Provavelmente,  você  irá  sesurpreender com o quanto você já realizou e oquanto se desenvolveu.
+// SCM Academy - marketing@secandidatemulher.com.br - IP: 187.92.92.99
+
+
+// 003 -> Módulo -> Conheça-te a Ti Mesma 
+
+// ----------------------------------------------------------------
+// 11 O que é uma análise comportamental
+
+// Avaliação dos comportamentos dos indivíduos. Ela estuda as características presentes em cada um de nós e a forma como reagimos a estímulos e agimos diante das mais variadas situações!
+
+// Testes que traçam um perfil que seleciona ou não um candidato
+
+// ----------------------------------------------------------------
+// 12 A importância do Autoconhecimento nos processos seletivos
+
+// "Pra quem não sabe o que quer, qualquer coisa serve".
+// Já mencionamos anteriormente que autoconhecimento é o que temdiferenciado quem passa em um processo de quem não passa. É a chavedesde a candidatura a entrevista final
+
+// Se não entendemos o que queremos, o que é importante para mim, o que faz sentido. Onde quero chegar e o que eu quero ser.
+
+// ----------------------------------------------------------------
+//* 13 Atividade - Teste 16 Personalidades
+//! Pendente:
+// O teste de 16 personalidades contém perguntas para determinar qual o tipo de personalidade você possui. Ela vai descrever quem você é e porque você faz as coisas da forma que faz. Ele leva, em média, 10 minutos e os seus usuários o descrevem como "muito preciso".
+
+// Com o teste, é possível entender as personalidades e o que as move, inspira e preocupa, te ajudando a construir relações mais significativas. Com ferramentas e testes disponíveis, a plataforma te ajuda a explorar como as  características da sua personalidade te afetam em vários aspectos da sua vida: sua confiança, perfeccionismo, burnout e mais!
+
+// https://www.16personalities.com/br 
+
+// ----------------------------------------------------------------
+// 14 Soft Skills x Had Skills 
+
+// As Soft Skills são, em linhas gerais, as habilidades comportamentais que umindivíduo possui na condução de suas atividades diárias. Ou seja: elas estãodiretamente ligadas à forma como lidamos com as pessoas ao nosso redor.As Hard Skills são as habilidades que podem ser facilmente aprendidas (eensinadas) por meio de cursos, treinamentos, workshops etc. De forma simples,podemos conceituá-las como as aptidões técnicas de um profissional.
+
+// Habilidades humanas e comportamentais
+// Soft -> o que sei fazer, aptidões desenvolvidas
+// Hard -> aptidões técnicas, o que entregou  ou onde se especializou.
+// Importante descobrir quais as habilidades para o seu propósito de carreira
+
+// ----------------------------------------------------------------
+// 15 Análise SWOT Pessoal
+
+//! Pendente
+// Atividade - SWOT Pessoal  (Material na Pasta SCM)
+// Forças / Fraquezas / Oportunidades / Ameaças
+
+// 004 -> Módulo -> O Que Ninguém te Conta Sobre Currículo 
+
+// ----------------------------------------------------------------
+// 16 Antes de tudo, o que você precisa saber sobre currículo
+
+// Focado
+// Currículo específico para cada vaga
+// rotina para Candidaturas
+// Júnior -> Bom currículo de 1 página
+// Se for Plena e Sênior -> Bom currículo resumindo o foco para aquela vaga.
+// Péssimo ter um modelo só, pois precisamos adaptar por plataforma
+// Primeira avaliação será dos Robôs (Filtro)
+// O Robô compara Informações, Competências e Habilidades que o recrutador determinou antes de divulgar a vaga
+// Passando nessa peneira, já temos um primeiro "met da vaga" e você pode ser convidado para a próxima vaga.
+
+
+// ----------------------------------------------------------------
+//  A Influência da Inteligência Artificial nos processos seletivos
+// !Parei aqui 11-01-24
+
+// ----------------------------------------------------------------
+
+// ----------------------------------------------------------------
+
+// ----------------------------------------------------------------
+
+// ----------------------------------------------------------------
+
+// ----------------------------------------------------------------
+
+// ----------------------------------------------------------------
+
+// ----------------------------------------------------------------
+
+// ----------------------------------------------------------------
+
+// ----------------------------------------------------------------
+
+// ----------------------------------------------------------------
+
+// ----------------------------------------------------------------
+
+// ----------------------------------------------------------------
+
+// ----------------------------------------------------------------
+
+// ----------------------------------------------------------------
+
+// ----------------------------------------------------------------
+
+// ----------------------------------------------------------------
+
+
+
+
+
+
+
+
